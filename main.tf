4476356d0d7186a287a23e03f2953284792059a1
saikumar <dsaikumar2000@gmail.com>
2026-08-08 09:42:58 +0000

    main folder added

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "google_storage_bucket" "tf_state_bucket" {
   name          = "my-terraform-backend-bucket"
   location      = "US"
   storage_class = "STANDARD"
-
   uniform_bucket_level_access = true
 }
+### sai
